c38d2d6ddb552a75fc251881f0a0293292e55bb7
Jiří Veverka <jiri.veverka@tbs-biometrics.com>
2023-02-28 13:56:56 +0100

    Prepare testing infrastructure for adc
    Signed-off-by: Jiří Veverka <jiri.veverka@tbs-biometrics.com>

--- adc_light.c
+++ adc_light.c
@@ -1,5 +1,10 @@
 #include "adc_light.h"
 
-void get_adc_val() {
+void init_adc_light() {
     return;
 }
+
+uint8_t get_adc_val(uint8_t sensor_num) {
+    return sensor_num;
+}
+
--- adc_light.h
+++ adc_light.h
@@ -1,6 +1,9 @@
 #ifndef ADC_LIGHT
 #define ADC_LIGHT
 
-void get_adc_val();
+#include "libs.h"
+
+void init_adc_light();
+uint8_t get_adc_val(uint8_t sensor_num);
 
 #endif
