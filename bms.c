ac0c1c5ee3e601c1f58ca10064033107f87a9277
Jiří Veverka <jiri.veverka@tbs-biometrics.com>
2023-03-20 16:56:23 +0100

    BMS: Prepare the .c and .h files for future work
    Signed-off-by: Jiří Veverka <jiri.veverka@tbs-biometrics.com>

---
+++ bms.c
@@ -0,0 +1,8 @@
+#include "bms.h"
+
+/*TODO: Design a battery management system (BMS) which will:
+        - Read the actual current and voltage of the battery
+        - Read the voltage input of the solar panels
+        - Read the battery current output
+        - Turn on/off the RPi based on the battery current and voltage level
+*/
